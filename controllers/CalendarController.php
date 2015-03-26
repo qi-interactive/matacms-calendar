c05a720c4fae3e50b28ebf76e522e9e371f0c117
Michal Fiedorowicz <michal@qi-interactive.com>
2015-03-26 18:01:26 +0000

    Update

--- controllers/CalendarController.php
+++ controllers/CalendarController.php
@@ -8,6 +8,16 @@ use matacms\controllers\module\Controller;
 class CalendarController extends Controller 
 {
 
+	public function getModel() 
+    {
+        return null;
+    }
+
+    public function getSearchModel() 
+    {
+        return null;
+    }
+    
     public function actionIndex() {
 
         return $this->render("index");
